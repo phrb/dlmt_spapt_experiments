91b6f442ac51f2d4678235fd2b3aca2d77d4d581
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-08 17:56:54 +0200

    Fix stencil3d model parameters

--- orio/testsuite/SPAPT/stencil3d/stencil3d.src1.c
+++ orio/testsuite/SPAPT/stencil3d/stencil3d.src1.c
@@ -78,9 +78,8 @@
     arg dlmt_extra_experiments = 6;
     arg dlmt_design_multiplier = 1.3;
     arg dlmt_steps = 4;
-    arg dlmt_linear = '["SCR", "VEC1", "VEC2", "OMP1", "OMP2"]';
+    arg dlmt_linear = '["SCR", "VEC1", "VEC2", "OMP1", "OMP2", "RT1_I", "RT2_I", "RT1_J", "RT1_K", "RT2_J", "RT2_K", "T1_I", "T1_Ia", "T2_I", "T2_Ia", "U1_I", "U2_I", "T1_J", "T1_Ja", "T1_K", "T1_Ka", "T2_J", "T2_Ja", "T2_K", "T2_Ka", "U1_J", "U1_K", "U2_J", "U2_K"]';
     arg dlmt_quadratic = '["RT1_I", "RT2_I", "RT1_J", "RT1_K", "RT2_J", "RT2_K", "T1_I", "T1_Ia", "T2_I", "T2_Ia", "U1_I", "U2_I", "T1_J", "T1_Ja", "T1_K", "T1_Ka", "T2_J", "T2_Ja", "T2_K", "T2_Ka", "U1_J", "U1_K", "U2_J", "U2_K"]';
-    arg dlmt_linear = '["RT1_I", "RT2_I", "RT1_J", "RT1_K", "RT2_J", "RT2_K", "T1_I", "T1_Ia", "T2_I", "T2_Ia", "U1_I", "U2_I", "T1_J", "T1_Ja", "T1_K", "T1_Ka", "T2_J", "T2_Ja", "T2_K", "T2_Ka", "U1_J", "U1_K", "U2_J", "U2_K"]';
   }
 
   def input_params
