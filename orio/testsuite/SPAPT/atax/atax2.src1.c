686af33f41e886744d4f4e6ffb59d7be5d22cd5f
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-10 16:09:48 +0200

    Update cubic factors

--- orio/testsuite/SPAPT/atax/atax2.src1.c
+++ orio/testsuite/SPAPT/atax/atax2.src1.c
@@ -21,7 +21,7 @@
     arg dlmt_aov_threshold = 0.01;
     arg dlmt_linear = '["ACOPY_x", "ACOPY_y", "SCR", "VEC1", "VEC2", "T1_I", "T1_J", "T1_K", "T2_I", "T2_J", "T2_K", "U1_I", "U_I", "U_J", "U_K", "RT_I", "RT_J", "RT_K"]';
     arg dlmt_quadratic = '["T1_I", "T1_J", "T1_K", "T2_I", "T2_J", "T2_K", "U1_I", "U_I", "U_J", "U_K", "RT_I", "RT_J", "RT_K"]';
-    arg dlmt_cubic = '["T1_I", "T1_J", "T1_K", "T2_I", "T2_J", "T2_K", "U1_I", "U_I", "U_J", "U_K", "RT_I", "RT_J", "RT_K"]';
+    arg dlmt_cubic = '["T1_I", "T1_J", "T1_K", "T2_I", "T2_J", "T2_K", "U1_I", "U_I", "U_J", "U_K"]';
   }
 
   def performance_params
