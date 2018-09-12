304974dee7f8f98e1ec986c003d4840047949639
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-12 13:38:10 +0200

    Update atax experiment

--- orio/testsuite/SPAPT/atax/atax2.src1.c
+++ orio/testsuite/SPAPT/atax/atax2.src1.c
@@ -13,14 +13,14 @@
   def search
   {
     arg algorithm = 'DLMT';
-    arg total_runs = 75;
     arg dlmt_federov_sampling = 100;
     arg dlmt_extra_experiments = 2;
     arg dlmt_design_multiplier = 1.5;
-    arg dlmt_steps = 12;
-    arg dlmt_aov_threshold = 0.01;
+    arg dlmt_steps = 4;
+    arg dlmt_aov_threshold = 0.05;
     arg dlmt_linear = '["ACOPY_x", "ACOPY_y", "SCR", "VEC1", "VEC2", "T1_I", "T1_J", "T1_K", "T2_I", "T2_J", "T2_K", "U1_I", "U_I", "U_J", "U_K", "RT_I", "RT_J", "RT_K"]';
     arg dlmt_quadratic = '["T1_I", "T1_J", "T1_K", "T2_I", "T2_J", "T2_K", "U1_I", "U_I", "U_J", "U_K", "RT_I", "RT_J", "RT_K"]';
+    arg dlmt_cubic = '["T1_I", "T1_J", "T1_K", "T2_I", "T2_J", "T2_K", "U1_I", "U_I", "U_J", "U_K", "RT_I", "RT_J", "RT_K"]';
   }
 
   def performance_params
