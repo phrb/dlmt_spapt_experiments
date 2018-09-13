5a3f820a3f89df5255bb09c51e827abbc4e7bc5f
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-13 18:13:54 +0200

    Thu Sep 13 18:13:54 CEST 2018

--- orio/testsuite/SPAPT/atax/atax2.c
+++ orio/testsuite/SPAPT/atax/atax2.c
@@ -20,7 +20,8 @@
     arg dlmt_steps = 4;
     arg dlmt_aov_threshold = 0.05;
     arg dlmt_linear = '["ACOPY_x", "ACOPY_y", "SCR", "VEC1", "VEC2", "T1_I", "T1_J", "T1_K", "T2_I", "T2_J", "T2_K", "U1_I", "U_I", "U_J", "U_K", "RT_I", "RT_J", "RT_K"]';
-    arg dlmt_quadratic = '["T1_I", "T1_J", "T1_K", "T2_I", "T2_J", "T2_K", "U1_I", "U_I", "U_J", "U_K", "RT_I", "RT_J", "RT_K"]';
+    #arg dlmt_quadratic = '["T1_I", "T1_J", "T1_K", "T2_I", "T2_J", "T2_K", "U1_I", "U_I", "U_J", "U_K", "RT_I", "RT_J", "RT_K"]';
+    #arg dlmt_cubic = '["T1_I", "T1_J", "T1_K", "T2_I", "T2_J", "T2_K", "U1_I", "U_I", "U_J", "U_K", "RT_I", "RT_J", "RT_K"]';
   }
 
   def performance_params
