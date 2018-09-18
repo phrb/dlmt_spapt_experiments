e4fad1148f67cc72b613cbe46a445d49adf4d156
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-18 18:59:55 +0200

    Tue Sep 18 18:59:55 CEST 2018

--- orio/testsuite/SPAPT/stencil3d/stencil3d.c
+++ orio/testsuite/SPAPT/stencil3d/stencil3d.c
@@ -73,15 +73,15 @@
   def search
   {
     arg algorithm = 'DLMT';
-    arg total_runs = 100;
+    arg total_runs = 1;
     arg dlmt_federov_sampling = 20;
-    arg dlmt_extra_experiments = 4;
-    arg dlmt_design_multiplier = 1.5;
-    arg dlmt_steps = 4;
+    arg dlmt_extra_experiments = 1;
+    arg dlmt_design_multiplier = 1;
+    arg dlmt_steps = 1;
     arg dlmt_aov_threshold = 0.05;
     arg dlmt_linear = '["SCR", "VEC1", "VEC2", "OMP1", "OMP2", "RT1_I", "RT2_I", "RT1_J", "RT1_K", "RT2_J", "RT2_K", "T1_I", "T1_Ia", "T2_I", "T2_Ia", "U1_I", "U2_I", "T1_J", "T1_Ja", "T1_K", "T1_Ka", "T2_J", "T2_Ja", "T2_K", "T2_Ka", "U1_J", "U1_K", "U2_J", "U2_K"]';
     arg dlmt_quadratic = '["RT1_I", "RT2_I", "RT1_J", "RT1_K", "RT2_J", "RT2_K", "T1_I", "T1_Ia", "T2_I", "T2_Ia", "U1_I", "U2_I", "T1_J", "T1_Ja", "T1_K", "T1_Ka", "T2_J", "T2_Ja", "T2_K", "T2_Ka", "U1_J", "U1_K", "U2_J", "U2_K"]';
-    arg dlmt_cubic = '["RT1_I", "RT2_I", "RT1_J", "RT1_K", "RT2_J", "RT2_K", "T1_I", "T1_Ia", "T2_I", "T2_Ia", "U1_I", "U2_I", "T1_J", "T1_Ja", "T1_K", "T1_Ka", "T2_J", "T2_Ja", "T2_K", "T2_Ka", "U1_J", "U1_K", "U2_J", "U2_K"]';
+    #arg dlmt_cubic = '["RT1_I", "RT2_I", "RT1_J", "RT1_K", "RT2_J", "RT2_K", "T1_I", "T1_Ia", "T2_I", "T2_Ia", "U1_I", "U2_I", "T1_J", "T1_Ja", "T1_K", "T1_Ka", "T2_J", "T2_Ja", "T2_K", "T2_Ka", "U1_J", "U1_K", "U2_J", "U2_K"]';
   }
 
   def input_params
