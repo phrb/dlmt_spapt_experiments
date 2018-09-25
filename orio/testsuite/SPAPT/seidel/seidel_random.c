c2b3472ed7a5bb20dfdb89c63aec4308b25d16f6
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-25 12:14:53 +0200

    Tue Sep 25 12:14:53 CEST 2018

--- orio/testsuite/SPAPT/seidel/seidel_random.c
+++ orio/testsuite/SPAPT/seidel/seidel_random.c
@@ -1,37 +1,36 @@
-/*@ begin PerfTuning (          
-  def build  
-  {  
-  arg build_command = 'gcc -O3 -fopenmp -DDYNAMIC'; 
+/*@ begin PerfTuning (
+  def build
+  {
+  arg build_command = 'timeout --kill-after=30s --signal=9 20m gcc -O3 -fopenmp -DDYNAMIC';
   arg libs = '-lm -lrt';
   }
-     
-  def performance_counter           
-  {  
-    arg repetitions = 35;  
-  } 
- 
-  def performance_params  
-  { 
+
+  def performance_counter
+  {
+    arg repetitions = 10;
+  }
+
+  def performance_params
+  {
 
 
     # Cache tiling
-    param T1_T[] = [1,16,32,64,128,256,512];
-    param T1_I[] = [1,16,32,64,128,256,512];
-    param T1_J[] = [1,16,32,64,128,256,512];
+    param T1_T[] = [1,2,4,8,16,32,64,128,256,512,1024,2048];
+    param T1_I[] = [1,2,4,8,16,32,64,128,256,512,1024,2048];
+    param T1_J[] = [1,2,4,8,16,32,64,128,256,512,1024,2048];
+    param T1_Ta[] = [1,2,4,8,16,32,64,128,256,512,1024,2048];
+    param T1_Ia[] = [1,2,4,8,16,32,64,128,256,512,1024,2048];
+    param T1_Ja[] = [1,2,4,8,16,32,64,128,256,512,1024,2048];
 
-    param T1_Ta[] = [1,64,128,256,512,1024,2048];
-    param T1_Ia[] = [1,64,128,256,512,1024,2048];
-    param T1_Ja[] = [1,64,128,256,512,1024,2048];
-
-    # Unroll-jam 
+    # Unroll-jam
     param U1_T[]  = range(1,31);
     param U1_I[]  = range(1,31);
     param U1_J[]  = range(1,31);
 
     # Register tiling
-    param RT1_I[] = [1,8,32];
-    param RT1_J[] = [1,8,32];
-    param RT1_T[] = [1,8,32];
+    param RT1_I[] = [1,2,4,8,16,32];
+    param RT1_J[] = [1,2,4,8,16,32];
+    param RT1_T[] = [1,2,4,8,16,32];
 
     # Scalar replacement
     param SCR[]  = [False,True];
@@ -46,29 +45,31 @@
     constraint tileI = ((T1_Ia == 1) or (T1_Ia % T1_I == 0));
     constraint tileJ = ((T1_Ja == 1) or (T1_Ja % T1_J == 0));
     constraint tileT = ((T1_Ta == 1) or (T1_Ta % T1_T == 0));
-
     constraint reg_capacity = (RT1_I*RT1_J + RT1_I*RT1_T + RT1_J*RT1_T <= 150);
     constraint unroll_limit = ((U1_I == 1) or (U1_J == 1) or (U1_T == 1));
-
-  } 
- 
-  def search  
-  {
-    arg algorithm = 'Randomsearch';  
-    arg total_runs = 10000; 
   }
 
-  def input_params  
+  def search
   {
-    param T[] = [50];
-    param N[] = [500];
-  } 
+    arg algorithm = 'Randomsearch';
+    arg total_runs = 300;
+  }
+
+  def input_params
+  {
+    param T[] = [200];
+    param N[] = [1000];
+  }
 
   def input_vars
   {
     decl static double A[N][N+17] = random;
   }
-) @*/  
+
+  def validation {
+    arg validation_file = 'validation_3x.c';
+  }
+) @*/
 
 
 #define max(x,y)    ((x) > (y)? (x) : (y))
@@ -95,10 +96,10 @@ for (t=0; t<=T-1; t++)
   for (i=1; i<=N-2; i++)
     for (j=1; j<=N-2; j++)
       A[i][j] = (A[i-1][j-1] + A[i-1][j] + A[i-1][j+1]
-		 + A[i][j-1] + A[i][j] + A[i][j+1]
-		 + A[i+1][j-1] + A[i+1][j] + A[i+1][j+1])/9.0;
+         + A[i][j-1] + A[i][j] + A[i][j+1]
+         + A[i+1][j-1] + A[i+1][j] + A[i+1][j+1])/9.0;
 ) @*/
 
-/*@ end @*/ 
-/*@ end @*/ 
+/*@ end @*/
+/*@ end @*/
 
