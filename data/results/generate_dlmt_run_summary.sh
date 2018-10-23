3902126a4bdaa07604b2e833f93dce686fe3aade
Pedro Bruel <pedro.bruel@gmail.com>
2018-10-23 13:41:42 +0200

    Fix extra columns

--- data/results/generate_dlmt_run_summary.sh
+++ data/results/generate_dlmt_run_summary.sh
@@ -10,48 +10,57 @@ for f in $DLMT_FILES; do
     RUN_ID="$(echo "$f" | cut -d_ -f6 | cut -d/ -f1),"
     KERNEL="$(echo "$f" | cut -d_ -f6 | cut -d/ -f2 | cut -d. -f1)"
     TABLE+="$KERNEL,$RUN_ID"
-    TABLE+="$(grep "D-Eff" $f | cut -d" " -f4 | tr $'\n' ',')"
 
     ITEMS=$(grep "D-Eff" $f | wc -l)
-    MISSING=$(expr $ITERATIONS - $ITEMS)
+    if [[ "$ITEMS" -gt 0 ]]
+    then
+        TABLE+="$(grep "D-Eff" $f | cut -d" " -f4 | tr $'\n' ',')"
+    fi
 
-    if [ $MISSING -gt 0 ]
+    MISSING=$(expr $ITERATIONS - $ITEMS)
+    if [[ $MISSING -gt 0 ]]
     then
         for (( i=1; i<=$MISSING; i++ )); do
             TABLE+=","
         done
     fi
 
-    TABLE+="$(grep "Predicting Best Values for:" $f | cut -d"[" -f2 | cut -d"]" -f1 | sed "s/^/\"/" | sed "s/\$/\"/" | sed "s/,/:/g" | tr $'\n' ',')"
+    ITEMS=$(grep "Predicting Best Values for:" $f | wc -l)
+    if [[ "$ITEMS" -gt 0 ]]
+    then
+        TABLE+="$(grep "Predicting Best Values for:" $f | cut -d"[" -f2 | cut -d"]" -f1 | sed "s/^/\"/" | sed "s/\$/\"/" | sed "s/,/:/g" | tr $'\n' ',')"
+    fi
 
-    ITEMS="$(grep "Predicting Best Values for:" $f | wc -l)"
     MISSING=$(expr $ITERATIONS - $ITEMS)
-
-    if [ $MISSING -gt 0 ]
+    if [[ $MISSING -gt 0 ]]
     then
         for (( i=1; i<=$MISSING; i++ )); do
             TABLE+=","
         done
     fi
 
-    TABLE+="$(grep "Slowdown (Design Best):" $f | cut -d" " -f4 | uniq | tr $'\n' ',')"
+    ITEMS=$(grep "Slowdown (Design Best):" $f | uniq | wc -l)
+    if [[ "$ITEMS" -gt 0 ]]
+    then
+        TABLE+="$(grep "Slowdown (Design Best):" $f | cut -d" " -f4 | uniq | tr $'\n' ',')"
+    fi
 
-    ITEMS="$(grep "Slowdown (Design Best):" $f | uniq | wc -l)"
     MISSING=$(expr $ITERATIONS - $ITEMS)
-
-    if [ $MISSING -gt 0 ]
+    if [[ $MISSING -gt 0 ]]
     then
         for (( i=1; i<=$MISSING; i++ )); do
             TABLE+=","
         done
     fi
 
-    TABLE+="$(grep "Slowdown (Predicted Best):" $f | cut -d" " -f4 | uniq | tr $'\n' ',' | sed "s/,\$//")"
+    ITEMS=$(grep "Slowdown (Predicted Best):" $f | uniq | wc -l)
+    if [[ "$ITEMS" -gt 0 ]]
+    then
+        TABLE+="$(grep "Slowdown (Predicted Best):" $f | cut -d" " -f4 | uniq | tr $'\n' ',' | sed "s/,\$//")"
+    fi
 
-    ITEMS="$(grep "Slowdown (Predicted Best):" $f | uniq | wc -l)"
-    MISSING=$(expr $ITERATIONS - $ITEMS)
-
-    if [ $MISSING -gt 0 ]
+    MISSING=$(expr $ITERATIONS - $ITEMS - 1)
+    if [[ $MISSING -gt 0 ]]
     then
         for (( i=1; i<=$MISSING; i++ )); do
             TABLE+=","
