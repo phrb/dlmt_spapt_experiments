6e19ce6064f29c086be31cb15f65dc2aee5ca822
Pedro Bruel <pedro.bruel@gmail.com>
2019-09-21 16:17:15 -0300

    Split experiments correctly

--- data/tests/list_python_errors.sh
+++ data/tests/list_python_errors.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/zsh
-find $1/bicgkernel/ -name "*.stderr" | xargs -I{} zsh -c 'echo {} && grep -B 2 -A 2 -n "Traceback" {}'
+find $1/bicgkernel/ -name "*.stderr" | xargs -I{} zsh -c 'echo {} && grep -B 5 -A 5 -n "Traceback" {}'
