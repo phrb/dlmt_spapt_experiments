2dabf0ce804dab944a0ceb76ad75ba687c657fd8
Pedro Bruel <pedro.bruel@gmail.com>
2019-12-17 11:32:58 -0800

    Change default kernel for GPR

--- orio/orio/main/tuner/search/gpr/gpr.py
+++ orio/orio/main/tuner/search/gpr/gpr.py
@@ -476,6 +476,7 @@ class GPR(orio.main.tuner.search.search.Search):
 
             gpr_model <- km(design = select(training_data, -cost_mean),
                             response = training_data$cost_mean,
+                            covtype = "exp",
                             multistart = 2 * cores,
                             control = list(pop.size = 400,
                                            BFGSburnin = 500))
